64951c3cab1083b9ccbc723227ab939762cfd07f
bilalmalik101 <144342543+bilalmalik101@users.noreply.github.com>
2023-10-22 17:10:26 +0500

    using while loop and conditional statements for computation and getting result

--- reverseguessinggame.c
+++ reverseguessinggame.c
@@ -7,4 +7,24 @@ int main(){
     printf("Give the Lowets and Highest Number:");
     scanf("%d%d",&lowestNum,&highestNum);
     printf("Pick a number between %d and %d to guess\n",lowestNum,highestNum);
+    while(decision!='c'){
+        guess=(highestNum + lowestNum)/3;
+        printf("The guess number as %d\n",guess);
+        printf("If the guess number as correct then press 'c' , Lowest then press 'l' , Highest press 'h':");
+        scanf(" %c",&decision);
+        if(decision=='l'){
+            highestNum+=2;
+        }
+        else if(decision=='h'){
+            lowestNum-=2;
+        }
+        else{
+            printf("Please tell them the number is correct or not\n");
+        }
+        
+        
+    }
+    printf("Congratulation you guess the number %d\n",guess);
+    return 0;
     
+}
